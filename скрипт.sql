56d4f3f4d2a3e27abf278b324a90a40f1253aba2
TodayIsMay <aira.1996@yandex.ru>
2022-03-31 10:34:36 +0300

    - import

--- скрипт.sql
+++ скрипт.sql
@@ -13,3 +13,18 @@ quantity INT,
 ask_price INT,
 creation_time VARCHAR (200)
 );
+CREATE TABLE symbols (
+id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR (5),
+ask INT,
+bid INT);
+INSERT INTO symbols (name, ask, bid)
+VALUES ('AAPL', 170, 168);
+INSERT INTO symbols (name, ask, bid)
+VALUES ('FB', 220, 216);
+INSERT INTO symbols (name, ask, bid)
+VALUES ('AMZN', 3300, 3297);
+INSERT INTO symbols (name, ask, bid)
+VALUES ('YNDX', 20, 18);
+INSERT INTO symbols (name, ask, bid)
+VALUES ('TSLA', 1000, 993);
